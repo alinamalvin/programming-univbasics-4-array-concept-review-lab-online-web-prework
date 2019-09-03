c2e08b37c8c135e1ece10735b0a2a2ed48ef5243
Alina Kornilova <alinakornilova78@gmail.com>
2019-09-03 18:44:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
-def find_element_index(array, value_to_find)
+def find_element_index(scale, value_to_find)
 counter=0 
-while array[counter] do
-  puts array[counter]
+while scale[counter] do
+  puts scale[counter]
   counter += 1
 end
 
