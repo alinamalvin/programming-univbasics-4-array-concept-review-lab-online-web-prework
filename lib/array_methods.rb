4da143149dfb3ca0e5a1af757f20ad0b8a2c419b
Alina Kornilova <alinakornilova78@gmail.com>
2019-09-03 18:15:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
 p scale('2')
-p a.scale('3')
-p a.scale('4')
-p a.scale('5')
+p scale('3')
+p scale('4')
+p scale('5')
 end
 
 def find_max_value(array)
